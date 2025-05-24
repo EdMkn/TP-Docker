d1209d6be27f6aa7eb18561d234b6b100263adf3
Manyim Ma Makon <nmaknyim@gmail.com>
2025-05-24 02:48:43 +0200

    Ajustements

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM tiangolo/uwsgi-nginx-flask:python3.6
+WORKDIR /app
+RUN apt update && apt install postgresql -y
+
+COPY . .
+RUN pip install -r requirements.txt
+RUN cp entrypoint.sh /docker-entrypoint.sh
+
+USER root
+EXPOSE 5000
+RUN chmod 750 /docker-entrypoint.sh
+ENTRYPOINT ["/docker-entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+# Wait for PostgreSQL to be ready
+echo "Waiting for PostgreSQL..."
+until pg_isready -h "$DBHOST" -p 5432 -U "$DBUSER"; do
+  sleep 1
+done
+
+echo "PostgreSQL is ready"
+
 flask db upgrade 
 flask run -h 0.0.0.0 -p 5000
-
-export DBUSER=groupeX
-export DBPASS=groupeX
-export DBHOST=groupeX
-export DBNAME=groupeX
